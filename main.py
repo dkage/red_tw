5eaba5228daa9d0b0bf593ef0fb096c78a361054
Danilo Cunha <danilokage@gmail.com>
2019-05-08 20:25:22 -0300

    Clean main code by adding new function to store possible texts messages

--- main.py
+++ main.py
@@ -4,8 +4,23 @@ from time import sleep
 from telegram_bot import Telegram
 
 
-# Keeps script running and getting new updates
+def text_messages(option, env=''):
+    """ This function returns the corresponding message to be sent for user through Telegram bot. """
+    if option == 1:
+        return 'Action Reddit to Twitter process cancelled by user.'
+    elif option == 2:
+        return 'Reddit link received. Please choose account to make upload, type "DEV" for test account or "PROD" for '\
+               'main account.'
+    elif option == 3:
+        return 'Type the message to be tweeted alongside the media from Reddit\n' \
+                                     'the message will be sent using Twitter profile set as {} environment' \
+                                     ''.format(env)
+    return 'INVALID OPTION'
+
+
 def main():
+    """Keeps script running and getting new telegram bot updates"""
+
     link_received = 0
     ready_to_tweet = 0
     reddit_link = ''
@@ -16,14 +31,14 @@ def main():
 
         for update in last_updates:
             update_data = bot.get_update_data(update['message'])
-            message_to_be_sent = ''
+
             # Debugs
             # print(update_data['id'])
             # print(update_data['user'])
             # bot.send_message(update_data['text'], update_data['id)
 
             if 'CANCEL' in update_data['text']:
-                message_to_be_sent = 'Action Reddit to Twitter process cancelled by user.'
+                message_to_be_sent = text_messages(1)
                 link_received = 0
                 reddit_link = ''
 
@@ -33,16 +48,13 @@ def main():
                 reddit_link = ''
 
             elif 'reddit.com' in update_data['text'] and link_received == 0:
-                message_to_be_sent = 'Reddit link received. ' \
-                                     'Please choose account to make upload, type DEV for test account'
+                message_to_be_sent = text_messages(2)
                 link_received = 1
                 reddit_link = update_data['text']
 
             elif ('DEV' in update_data['text'] or 'dev' in update_data['text']) or \
                     ('PROD' in update_data['text'] or 'prod' in update_data['text']) and link_received == 1:
-                message_to_be_sent = 'Type the message to be tweeted alongside the media from Reddit\n' \
-                                     'the message will be sent using Twitter profile set as {} environment' \
-                                     ''.format(update_data['text'])
+                message_to_be_sent = text_messages(3, update_data['text'])
                 ready_to_tweet = 1
                 env = update_data['text']
 
